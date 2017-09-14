70059e8ee2a8dba46e4b4f4134ff64a4c0e4580e
Andrew Carr <2metres@users.noreply.github.com>
2017-09-14 15:22:03 +1000

    Try loading css into the Javascript

--- code/partial/index.js
+++ code/partial/index.js
@@ -1,28 +1,17 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import styles from './partial.css';
 
-/**
- * The partial component
- *
- * @disable-docs
- */
 const Partial = ( page ) => (
-	<div className="textwrapper">
-		{ page._body }
-	</div>
+  <div className="textwrapper">{ page._body }</div>
 );
 
 
 Partial.propTypes = {
-	/**
-	 * _body: (test)(12)
-	 */
-	_body: PropTypes.node.isRequired,
+  _body: PropTypes.node.isRequired,
 };
 
-
 Partial.defaultProps = {};
-
 
 export default Partial;
