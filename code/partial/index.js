c4ea432fae01fcc055b60c9a72b38fa9d4dbfbf4
Andrew Carr <andrew@2metres.com>
2017-09-14 06:29:39 +0000

    remove css

--- code/partial/index.js
+++ code/partial/index.js
@@ -1,12 +1,9 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import styles from './partial.css';
-
 const Partial = ( page ) => (
   <div className="textwrapper">{ page._body }</div>
 );
-
 
 Partial.propTypes = {
   _body: PropTypes.node.isRequired,
